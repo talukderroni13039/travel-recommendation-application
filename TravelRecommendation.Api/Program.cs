2277f20ae364ad7954d36685187c8a84abdd224f
YunusAliRony <yunusali.rony@techjays.com>
2025-12-02 22:43:27 +0600

    created clean architecture for set up and added swagger

--- TravelRecommendation.Api/Program.cs
+++ TravelRecommendation.Api/Program.cs
@@ -11,16 +11,16 @@ namespace TravelRecommendation
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
+           //  builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
